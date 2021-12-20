de0e0513111fa8c21c867e6631c87729ec2899f1
wushaojie2070 <wushaojie2070@gmail.com>
2021-12-21 00:20:56 +0800

    地图完善(评论内容未添加,地图组件呼出按钮未添加,地址标签未添加)

--- 411shortvideo/App.vue
+++ 411shortvideo/App.vue
@@ -1,27 +1,54 @@
 <script>
-export default {
-	onLaunch: function() {
-		console.log('App Launch');
+	export default {
+		globalData: {
+					updateApp: {
+						id: null,
+						path: null,
+					},
+					localplace:'暂未设置地址',
+				},
+		onLaunch: function() {
+			console.log('App Launch');
 
-		setTimeout(() => {
-			uni.setTabBarBadge({
-				index: 1,
-				text: '31'
+			setTimeout(() => {
+				uni.setTabBarBadge({
+					index: 1,
+					text: '31'
+				});
+				uni.showTabBarRedDot({
+					index: 3
+				});
+			}, 1000);
+			uni.getSetting({
+				success(res) {
+					if (!res.authSetting['scope.userLocation']) {
+						// 未授权
+						uni.authorize({
+							scope: 'scope.userLocation',
+							success() { //1.1 允许授权
+								uni.getLocation()
+
+							},
+							fail() { //1.2 拒绝授权
+								console.log("你拒绝了授权，无法获得周边信息")
+							}
+						})
+					} else {
+						// 已授权 ..(获取位置信息)
+					}
+				}
 			});
-			uni.showTabBarRedDot({
-				index: 3
-			});
-		}, 1000);
-	},
-	onShow: function() {
-		console.log('App Show');
-	},
-	onHide: function() {
-		console.log('App Hide');
-	}
-};
+
+		},
+		onShow: function() {
+			console.log('App Show');
+		},
+		onHide: function() {
+			console.log('App Hide');
+		}
+	};
 </script>
 
 <style>
-/*每个页面公共css */ 
+	/*每个页面公共css */
 </style>
--- 411shortvideo/pages/tabbar/tabbar-1/components/Map.vue
+++ 411shortvideo/pages/tabbar/tabbar-1/components/Map.vue
@@ -1,51 +1,100 @@
 <template>
-    <view>
-        <page-head :title="title"></page-head>
-        <view class="uni-common-mt">
-			<view style="text-align: center; border: 3px solid green;">请选择当前位置</view>
-            <view>
-                <map :latitude="latitude" :longitude="longitude" :markers="covers">
-                </map>
-            </view>
-        </view>
-        <!-- <image src="../../static/app-plus/location@3x.png"></image> -->
-    </view>
+	<view>
+		<page-head :title="title"></page-head>
+		<view class="box">
+			<!-- <view @click="getlocation()" style="text-align: center; border: 3px solid green;">请选择当前位置</view> -->
+			<view class="map">
+				<!-- <input style="text-align: center; border: 3px solid green;height: auto;" v-model="value" 
+				placeholder="请点击下方地图选择您的当前位置" :type="type" :border="border" @click="inputHandle"></input> -->
+				<view style="text-align: center; border: 3px solid green;height: auto;" :type="type" :border="border" @click="inputHandle">
+				{{ value == '' ? '请点击下方地图选择您的当前位置' : '您的当前位置为:'+value }}</view> 
+				<map v-show="" @tap="clickHandle" id="map" :latitude="latitude" :longitude="longitude" :markers="markers"></map>
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                title: 'map',
-                latitude: 29.887006604013127,
-                longitude: 121.481766,
-                covers: [{
-                    latitude: 39.9085,
-                    longitude: 116.39747,
-                    // #ifdef APP-PLUS
-                    // iconPath: '../../static/app-plus/location@3x.png',
-                    // #endif
-                    // #ifndef APP-PLUS
-                    // iconPath: '../../static/location.png',
-                    // #endif
-                }]
-            }
-        },
-        methods: {
-			
+	export default {
+		data() {
+			return {
 
-        }
-    }
+				value: uni.getStorageSync('address') ? uni.getStorageSync('address') : '',
+				type: 'text',
+				border: true,
+				latitude: '',
+				longitude: '',
+				markers: [{
+					latitude: '',
+					longitude: '',
+					// latitude: 29.887006604013127,
+					// longitude: 121.481766,
+
+					// iconPath: '../../static/imgs/bg.png'
+				}],
+			}
+		},
+		// onShow(params) {
+		// 	console.log("12435463"+this.value)
+		// 	var value = JSON.parse(decodeURIComponent(params.value));
+		// 	this.value = value;
+		// 	console.log("12435463"+value)
+		// },
+		created() {
+			this.onLoadHadnle();
+			this.value = getApp().globalData.localplace
+		},
+		methods: {
+			getlocation() {},
+			frame() {
+				uni.chooseLocation({
+					success: res => {
+						this.latitude = res.latitude;
+						this.longitude = res.longitude;
+						this.value = res.address;
+						this.markers[0].latitude = res.latitude;
+						this.markers[0].longitude = res.longitude;
+						getApp().globalData.localplace = res.address;
+						// console.log("地址::::"+res.address)
+						// this.markers[0].iconPath = '../../static/imgs/map.png';
+					}
+				});
+			},
+			onLoadHadnle(params) {
+				uni.getLocation({
+					type: 'wgs84',
+					success: res => {
+						this.longitude = res.longitude;
+						this.latitude = res.latitude;
+						this.markers[0].latitude = res.latitude;
+						this.markers[0].longitude = res.longitude;
+						// this.markers[0].iconPath = '../../static/imgs/map.png';
+					}
+				});
+			},
+			inputHandle(e) {
+				this.frame();
+			},
+			clickHandle(e) {
+				this.frame();
+			}
+
+		},
+	}
 </script>
 
 
-<style>
-    map {
-		margin-top: 60rpx;
-        width: 100%;
-        height: 300rpx;
-    }
-	uni-common-mt {
-		
+<style lang="scss" scoped>
+	.box {
+		height: 100vh;
+		.map {
+			width: 100%;
+			height: 100%;
+
+			map {
+				width: 100%;
+				height: 100%;
+			}
+		}
 	}
 </style>
