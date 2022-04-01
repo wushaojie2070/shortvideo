9f96a97ea8f309f3fff168bffdcd3dd081531d95
Cjh <1278253523@qq.com>
2022-04-01 21:58:02 +0800

    传视频（未完成）

--- 411shortvideo/pages/tabbar-3-detial/tabbar-3-video/tabbar-3-video.vue
+++ 411shortvideo/pages/tabbar-3-detial/tabbar-3-video/tabbar-3-video.vue
@@ -1,29 +1,42 @@
 <template>
-	<view class="content">
-		页面 - 发视频
-	</view>
+    <view>
+		<view>标题</view>
+		<input type="text" value="" placeholder="请输入你的标题"/>
+			<htz-image-upload 
+			:max="3" 
+			v-model="ceshiData" 
+			@uploadSuccess="ceshiUploadSuccess" 
+			action="https://skrvideo.fun/upload"
+			></htz-image-upload>
+    </view>
 </template>
-
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+    import htzImageUpload from '@/components/htz-image-upload/htz-image-upload.vue'
+    export default {
+        data() {
+            return {
+                ceshiData: [],
+            }
+        },
+        onLoad() {
 
-		},
-		methods: {
-
-		}
-	}
+        },
+        components: {
+            htzImageUpload,
+        },
+        methods: {
+			ceshiUploadSuccess(res) { //上传成功
+				var _res = JSON.parse(res.data);
+				console.log(_res)
+				console.log(res.status)
+				console.log("判断",_res.status == 200)
+				if (_res.status == 200) {
+					let urlid = "http://skrvideo.fun:9000"+_res.data.slice(21)
+					console.log("---------",urlid)
+					this.ceshiData.push(urlid)
+				}
+				console.log(this.ceshiData)
+			},
+        }
+    }
 </script>
-
-<style>
-	.content {
-		text-align: center;
-		height: 400upx;
-		margin-top: 200upx;
-	}
-</style>
--- 411shortvideo/pages/tabbar/tabbar-3/tabbar-3.vue
+++ 411shortvideo/pages/tabbar/tabbar-3/tabbar-3.vue
@@ -1,6 +1,5 @@
 <template>
 	<view class="content" :class="{'active':active}">
-		<image class="logo" :class="{'active':active}" src="../../../static/logo.png"  mode="aspectFit"></image>
 		<view class="tabbar-box-wrap">
 			<view class="tabbar-box">
 				<view class="tabbar-box-item" @click="goToPage('/pages/tabbar-3-detial/tabbar-3-release/tabbar-3-release')">
@@ -50,7 +49,7 @@ export default {
 <style lang="scss" scoped>
 .content {
 	display: flex;
-	align-items: center;
+	// align-items: center;
 	justify-content: center;
 	width: 100%;
 	/* #ifdef H5 */
@@ -65,26 +64,12 @@ export default {
 	&.active {
 		opacity: 1;
 	}
-	.logo {
-		position: relative;
-		margin-top: -400upx;
-		width: 200upx;
-		height: 200upx;
-		// z-index: -1;
-		opacity: 0;
-		transition: opacity 0.3s;
-		&.active {
-			opacity: 1;
-		}
-	}
 }
 .tabbar-box-wrap {
 	position: absolute;
 	width: 100%;
 	padding: 50upx;
 	box-sizing: border-box;
-	bottom: 0;
-	left: 0;
 	.tabbar-box {
 		position: relative;
 		display: flex;
@@ -95,21 +80,6 @@ export default {
 		box-sizing: border-box;
 		z-index: 2;
 		box-shadow: 0px 2px 5px 2px rgba(0, 0, 0, 0.1);
-		&:after {
-			content: '';
-			position: absolute;
-			bottom: -16upx;
-			left: 0;
-			right: 0;
-			margin: auto;
-			width: 50upx;
-			height: 50upx;
-			transform: rotate(45deg);
-			background: #fff;
-			z-index: 1;
-			box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.1);
-			border-radius: 2px;
-		}
 		&:before {
 			content: '';
 			position: absolute;
